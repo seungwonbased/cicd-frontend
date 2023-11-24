9d63c8dce06790f1d50a6a50d87aeb543e9adc2b
Seungwon, Bae <87196501+seungwonbased@users.noreply.github.com>
2023-11-24 13:01:52 +0900

    Update BaseURL.js

--- src/api/BaseURL.js
+++ src/api/BaseURL.js
@@ -1,1 +1,1 @@
-export const URL = "a274d2000c9d34865afa52f348d78539-2025713247.us-west-2.elb.amazonaws.com";
+export const URL = "a274d2000c9d34865afa52f348d78539-2025713247.us-west-2.elb.amazonaws.com:5000";
